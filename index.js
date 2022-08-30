107d017c7c32ec96d46c9cc390218e92960f8feb
sumon-hossain <sumonengr.96@gmail.com>
2022-08-30 22:19:27 +0600

    (Responsiv navbar)

--- index.js
+++ index.js
@@ -11,16 +11,33 @@
 //     title.innerHTML = text.slice(0,index++);
 // },100)
 
-let arrow = document.getElementById('arrow');
+// let arrow = document.getElementById('arrow');
 
-window.onscroll = function(){
-    let scroll = scrollY;
+// window.onscroll = function(){
+//     let scroll = scrollY;
 
-    if(scroll > 300){
-        arrow.classList.add('active');
+//     if(scroll > 300){
+//         arrow.classList.add('active');
         
+//     }
+//     else{
+//         arrow.classList.remove('active');
+//     }
+// }
+
+const navToggler = document.querySelector('.nav-toggler');
+
+navToggler.addEventListener("click", navToggle);
+
+function navToggle(){
+    navToggler.classList.toggle("active");
+    const nav = document.querySelector(".nav");
+    nav.classList.toggle("open");
+
+    if(nav.classList.contains("open")){
+        nav.style.maxHeight = nav.scrollHeight + "px";
     }
     else{
-        arrow.classList.remove('active');
+        nav.removeAttribute("style");
     }
 }
