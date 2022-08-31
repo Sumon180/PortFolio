9cac77f510b1326e0501c2a5a88ef71c454da227
sumon-hossain <sumonengr.96@gmail.com>
2022-08-31 07:46:25 +0600

    Aorrw Solved

--- index.js
+++ index.js
@@ -1,29 +1,18 @@
 
 
-// let title = document.getElementById("count_title");
-// let text = "Web Developer & Desginer";
-// let index = 0;
+let arrow = document.getElementById('uparrow');
 
-// setInterval(function(){
-//     if(index > text.length){
-//         index = 0;
-//     }
-//     title.innerHTML = text.slice(0,index++);
-// },100)
+window.onscroll = function(){
+    let scroll = scrollY;
 
-// let arrow = document.getElementById('arrow');
-
-// window.onscroll = function(){
-//     let scroll = scrollY;
-
-//     if(scroll > 300){
-//         arrow.classList.add('active');
+    if(scroll > 100){
+        arrow.classList.add('active');
         
-//     }
-//     else{
-//         arrow.classList.remove('active');
-//     }
-// }
+    }
+    else{
+        arrow.classList.remove('active');
+    }
+}
 
 const navToggler = document.querySelector('.nav-toggler');
 
