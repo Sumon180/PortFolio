52fb2a004b5ba4c9036897c253258dce239c095a
sumon-hossain <sumonengr.96@gmail.com>
2022-09-12 11:18:55 +0600

    Banner setting

--- index.js
+++ index.js
@@ -1,33 +1,40 @@
-
 //*************************ARROW*************************** */
-let arrow = document.getElementById('uparrow');
+let arrow = document.getElementById("uparrow");
 
-window.onscroll = function(){
-    let scroll = scrollY;
+window.onscroll = function () {
+  let scroll = scrollY;
 
-    if(scroll > 100){
-        arrow.classList.add('active');
-        
-    }
-    else{
-        arrow.classList.remove('active');
-    }
-}
+  if (scroll > 100) {
+    arrow.classList.add("active");
+  } else {
+    arrow.classList.remove("active");
+  }
+};
 
 //*************************NAVBAR TOGGLAR*********************** */
-const navToggler = document.querySelector('.nav-toggler');
+const navToggler = document.querySelector(".nav-toggler");
 
 navToggler.addEventListener("click", navToggle);
 
-function navToggle(){
-    navToggler.classList.toggle("active");
-    const nav = document.querySelector(".nav");
-    nav.classList.toggle("open");
+function navToggle() {
+  navToggler.classList.toggle("active");
+  const nav = document.querySelector(".nav");
+  nav.classList.toggle("open");
 
-    if(nav.classList.contains("open")){
-        nav.style.maxHeight = nav.scrollHeight + "px";
-    }
-    else{
-        nav.removeAttribute("style");
-    }
+  if (nav.classList.contains("open")) {
+    nav.style.maxHeight = nav.scrollHeight + "px";
+  } else {
+    nav.removeAttribute("style");
+  }
 }
+
+// NAVBAR HIDE & SHOW
+$(document).ready(function () {
+  $(window).scroll(function () {
+    if (this.scrollY > 200) {
+      $(".header").addClass("sticky");
+    } else {
+      $(".header").removeClass("sticky");
+    }
+  });
+});
